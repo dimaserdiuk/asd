4f8937e82e2dfb9f53060396c0562cc2b83fcd25
dimaserdiuk <55205450+dimaserdiuk@users.noreply.github.com>
2019-09-26 21:45:00 +0300

    фыв
    фывфв

---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+namespace labka_1._2
+{
+    public partial class Form1 : Form
+    {
+        
+        double[,] arr = new double[3, 3];
+        double[,] arr1 = new double[3, 3];
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Text files(*.txt)|* txt|All files(*.*)|*.*";
+            
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileway = openFileDialog1.FileName;
+                string[] filetext = File.ReadAllLines(fileway).Take(10).ToArray();
+
+                // разобрать в массив
+                for (int i = 0; i < 3; i++)
+                {
+                    int[] row = filetext[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
+                    for (int j = 0; j < 3; j++)
+                    {
+                        arr[i, j] = row[j];
+                    }
+                }
+                // вывести в текстбокс
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        richTextBox2.Text += arr[i, j].ToString() + "\t";
+                    }
+
+                    richTextBox2.Text += Environment.NewLine;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileway = openFileDialog1.FileName;
+                string[] filetext = File.ReadAllLines(fileway).Take(10).ToArray();
+                
+                // разобрать в массив
+                for (int i = 0; i < 3; i++)
+                {
+                    int[] row = filetext[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
+                    for (int j = 0; j < 3; j++)
+                    {
+                        arr[i, j] = row[j];
+                    }
+                }
+                // вывести в текстбокс
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        richTextBox1.Text += arr[i, j].ToString() + "\t";
+                    }
+
+                    richTextBox1.Text += Environment.NewLine;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+       
+    }
+}
---
+++ Matrix.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labka_1._2
+{
+    public class Matrix
+    {
+        double[,] m;
+
+        public Matrix(int M, int N)
+        {
+            m = new double[M, N];
+        }
+        public int M
+        {
+            get { if (m == null) return 0;
+                return m.GetLength(0);
+            }
+        }
+        public int N
+        {
+            get { if (m == null) return 1;
+                return m.GetLength(1);
+            }
+        }
+        public double this[int i,int j]
+        {
+            get { return m[i, j]; }
+            set { m[i, j] = value; }
+        }
+        public static Matrix operator -(Matrix a,Matrix b)
+        {
+            return new Matrix(a.M - b.M, a.N - b.N);
+        }
+        public override string ToString()
+        {
+            string s=string.Empty;
+            for( int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    s += string.Format("{0}", m[i, j]);
+                }
+            }
+            return s;
+            
+            
+        }
+    }
+}
